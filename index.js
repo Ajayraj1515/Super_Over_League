3c607dc3b670ef8525db21f02e619d2cf3fe67c5
ajay <ajayraj3461@gmail.com>
2024-05-09 08:23:35 +0000

    coding_practice

---
+++ index.js
@@ -0,0 +1,15 @@
+const element = (
+  <div>
+    <h1 className='heading'>Super Over League</h1>
+    <img
+      src='https://assets.ccbp.in/frontend/react-js/rcb-img.png'
+      className='greeting'
+    />
+    <img
+      src='https://assets.ccbp.in/frontend/react-js/csk-img.png'
+      className='greeting'
+    />
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
